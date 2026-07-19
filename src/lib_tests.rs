f5d6e1b68df2f7522212bf6813cc9033adc4efba
tupe12334 <ofek.gabay.he@gmail.com>
2026-07-19 22:24:02 +0300

    Add tests for handling distinct crons and invalid expressions

--- src/lib_tests.rs
+++ src/lib_tests.rs
@@ -10,10 +10,51 @@ fn drops_redundant_crons() {
 }
 
 #[test]
+fn removes_supersets_when_a_more_specific_cron_arrives_later() {
+    let union = CronUnion::new(["*/30 * * * *", "0 * * * *"]).unwrap();
+
+    let crons: Vec<_> = union.iter().map(ToString::to_string).collect();
+
+    assert_eq!(crons, vec!["*/30 * * * *"]);
+}
+
+#[test]
 fn keeps_distinct_crons() {
     let union = CronUnion::new(["0 9 * * *", "0 17 * * *"]).unwrap();
 
     let crons: Vec<_> = union.iter().map(ToString::to_string).collect();
 
     assert_eq!(crons, vec!["0 9 * * *", "0 17 * * *"]);
+}
+
+#[test]
+fn preserves_six_field_expressions_and_exposes_the_schedule() {
+    let union = CronUnion::new(["0 */5 * * * *"]).unwrap();
+    let cron = union.iter().next().unwrap();
+
+    assert_eq!(cron.to_string(), "0 */5 * * * *");
+    assert_eq!(cron.schedule().to_string(), "0 */5 * * * *");
+}
+
+#[test]
+fn keeps_distinct_crons_when_seconds_differ() {
+    let union = CronUnion::new(["0 */5 * * * *", "30 */5 * * * *"]).unwrap();
+
+    let crons: Vec<_> = union.iter().map(ToString::to_string).collect();
+
+    assert_eq!(crons, vec!["0 */5 * * * *", "30 */5 * * * *"]);
+}
+
+#[test]
+fn keeps_distinct_crons_when_minutes_differ() {
+    let union = CronUnion::new(["0 0 9 * * *", "0 1 9 * * *"]).unwrap();
+
+    let crons: Vec<_> = union.iter().map(ToString::to_string).collect();
+
+    assert_eq!(crons, vec!["0 0 9 * * *", "0 1 9 * * *"]);
+}
+
+#[test]
+fn returns_an_error_for_invalid_expressions() {
+    assert!(union(["not a cron"]).is_err());
 }
